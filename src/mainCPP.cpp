712a0b0a8a2642fa56b266eb4cef4415de0bfbe6
SPavelVR <pavelsakulindev@mail.ru>
2025-08-13 10:35:49 +0300

    Готовый проект, библотеки glew и glfm надо отдельно поставить в папку extern/

---
+++ src/mainCPP.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+
+#ifndef GLEW_STATIC
+#define GLEW_STATIC
+#endif
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+int main() {
+    // 1. Инициализация GLFW
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Hellow OpenGL", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "No wondow!\n";
+        glfwTerminate();
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "No glew <<<\n";
+        glfwTerminate();
+        return 1;
+    }
+
+    glViewport(0, 0, 1280, 720);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+
+    std::cout << "hellow openGL\n";
+    return 0;
+}
